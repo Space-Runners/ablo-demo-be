b8a97c2ef836fbe70297a951069e74a701df5187
Bojan Tosic <bojan.tosic@gmail.com>
2023-09-01 18:17:01 +0200

    Ignore design tests for now

--- test/test.imports.ts
+++ test/test.imports.ts
@@ -3,25 +3,20 @@ import { testingConfig } from './typeorm.config.test';
 import { User } from '../src/user/user.entity';
 import { AuthModule } from '../src/auth/auth.module';
 import { Role } from '../src/user/role.entity';
-import { Template } from '../src/template/template.entity';
 import { Design } from '../src/design/design.entity';
 import { ConfigModule } from '@nestjs/config';
 import { DesignModule } from '../src/design/design.module';
 import { UserModule } from '../src/user/user.module';
-import { GeneratorModule } from '../src/generator/generator.module';
-import { TemplateModule } from '../src/template/template.module';
 import { ScheduleModule } from '@nestjs/schedule';
 import { MailModule } from '../src/mail/mail.module';
 
 export const imports = [
   ConfigModule.forRoot(),
   TypeOrmModule.forRoot(testingConfig),
-  TypeOrmModule.forFeature([Role, Template, User, Design]),
+  TypeOrmModule.forFeature([Role, User, Design]),
   ScheduleModule.forRoot(),
   AuthModule,
   DesignModule,
   MailModule,
   UserModule,
-  GeneratorModule,
-  TemplateModule,
 ];
